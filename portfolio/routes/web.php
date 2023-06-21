f15f184b4799d92cc79ebbf7dcb9ac7acb9747ff
H A S A N <hasan25042019@gmail.com>
2023-06-21 21:13:18 +0600

    Update web.php
    fg

--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\portfolioController;
-use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,12 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', [portfolioController::class, 'getPortfolio']);
+Auth::routes();
 
-// Route::middleware(['auth'])->group(function () {
+// Route::middleware('auth') -> group(function(){
 //     Route::get('/home', [portfolioController::class, 'getPortfolio']);
 // });
 
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
