14f3756f8bb3740f0210ee99aff45cce857887ac
H A S A N <hasan25042019@gmail.com>
2023-06-23 20:31:33 +0600

    Update portfolioController.php
    erre

--- portfolio/app/Http/Controllers/portfolioController.php
+++ portfolio/app/Http/Controllers/portfolioController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\abouts;
 use Illuminate\Http\Request;
 
 class portfolioController extends Controller
@@ -12,5 +12,18 @@ class portfolioController extends Controller
 
     function getUser(){
         return view('pages.profile');
+    }
+
+    function getBlogPage(){
+        $showData = abouts::all();
+        return view('pages.blog', compact('showData')); 
+    }
+    
+    function getBlog(Request $request){
+        $crud = new abouts();             //cruds model set under $crud variable
+        $crud -> head_line = $request -> head_line;         //$crud->name(database field name) & $request->name(form field name)
+        $crud -> discription = $request -> discription;        //$crud->email(database field email) & $request->email(form field email)
+        $crud -> save();
+        return redirect('/blogPage');
     }
 }
