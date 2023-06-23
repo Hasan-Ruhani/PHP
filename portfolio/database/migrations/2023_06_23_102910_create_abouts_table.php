2c501d4438492032c121a2c051deb7ef4e63346f
H A S A N <hasan25042019@gmail.com>
2023-06-23 20:31:27 +0600

    Create 2023_06_23_102910_create_abouts_table.php
    erre

---
+++ portfolio/database/migrations/2023_06_23_102910_create_abouts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('head_line');
+            $table->longText('discription');
+            $table->string('image')->default('default_image.jpg')->change();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
